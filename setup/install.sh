8e92089cf978eaa628e702903a248ec50604efc9
Fred Leclerc <fred.leclerc@gmail.com>
2021-07-09 18:11:09 -0400

    feat(vim): Added prettier support

--- setup/install.sh
+++ setup/install.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+echo "Creating local bin dir"
+mkdir -p ~/.local/bin
+
+echo "Installing vim-plug"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+echo "Installing lf"
+git clone git@github.com:gokcehan/lf.git ~/Source/lf
+cd ~/Source/lf && go install 
