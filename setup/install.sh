0679c51503eba876f861be094a1f4cbe3d774da0
Fred Leclerc <fred.leclerc@gmail.com>
2021-07-11 12:15:48 -0400

    feat(setup,git): Setup adjustments, interactive git commit

--- setup/install.sh
+++ setup/install.sh
@@ -3,9 +3,15 @@
 echo "Creating local bin dir"
 mkdir -p ~/.local/bin
 
+echo "Creating local source code dir"
+mkdir ~/Source
+
 echo "Installing vim-plug"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 echo "Installing lf"
 git clone git@github.com:gokcehan/lf.git ~/Source/lf
 cd ~/Source/lf && go install 
+
+
+# git config --global commit.template ~/.gitmessage
