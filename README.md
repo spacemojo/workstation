75305f16266c7ece759f68bded1c624ea2f2ee70
Fred Leclerc <fred.leclerc@gmail.com>
2021-07-18 08:19:58 -0400

    feat(git,vim): Ongoing improvements

--- README.md
+++ README.md
@@ -6,15 +6,13 @@ I am starting out at a new job, and when we talk new job, we talk new workstatio
 
 Obviously, I won't share what is confidential and directly related to my employer. So the scope of this is around the tooling I use for general software development, architecture, devops and documentation. This is gonna be fun.
 
-## Git
-
-* [gitmessage](git/gitmessage) : Message template for conventional commits
+## tmux, tmux-session
 
 ## Docker
 
 ## Rust
 
-## Go
+## GO
 
 ## AWS
 
--- setup/install.sh
+++ setup/install.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
 
-echo "Creating local bin dir"
-mkdir -p ~/.local/bin
-
-echo "Creating local source code dir"
-mkdir ~/Source
-
-echo "Installing vim-plug"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-echo "Installing lf"
-git clone git@github.com:gokcehan/lf.git ~/Source/lf
-cd ~/Source/lf && go install 
-
-echo "Linking git message template"
-ln -s ./git/gitmessage ~/.gitmessage
-
-# git config --global commit.template ~/.gitmessage
