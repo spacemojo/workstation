5b4b0fc0034f07208feb71beb692af3cd5d144e5
F. Boy McClerk <fred.leclerc@gmail.com>
2021-09-26 23:44:34 -0400

    fix(update): Reinstated brew install function that was deleted

--- lib/stdst8-functions.sh
+++ lib/stdst8-functions.sh
@@ -20,6 +20,23 @@ stdst8.apt_install() {
   done;
 }
 
+stdst8.brew_install() {
+  local pkgs=("$@")
+  for name in "${pkgs[@]}"; 
+  do 
+    local LOG="${LOCAL_LOG}/${name}";
+    echo "${ST8_PREFIX}Ckecking brew package ${name}" > "${LOG}";
+    if [[ -a "${LOCAL_CFG}/${name}" ]]; then
+      echo "${ST8_PREFIX}${name} installed, skipping"  > "${LOG}"; 
+    else 
+      echo "${ST8_PREFIX}Installing ${name}" > "${LOG}";
+      brew install "${name}";
+      echo "Installed" > "${LOCAL_CFG}/${name}";
+    fi
+    echo
+  done;
+}
+
 stdst8.snap_install() {
   local pkgs=("$@")
   for name in "${pkgs[@]}"; 
@@ -30,7 +47,7 @@ stdst8.snap_install() {
       echo "${ST8_PREFIX}${name} installed, skipping"  > "${LOG}"; 
     else 
       echo "${ST8_PREFIX}Installing ${name}" > "${LOG}";
-      sudo snap install "${name}" --classic
+      sudo snap install "${name}" --classic;
       echo "Installed" > "${LOCAL_CFG}/${name}";
     fi
     echo
